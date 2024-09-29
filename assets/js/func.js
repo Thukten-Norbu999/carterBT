9ce9e2db0151d6473ae0bf7c6bfeeece4eed9f6b
Thukten Norbu <thuktennorbu33@gmail.com>
2024-09-30 04:09:46 +0600

    done

--- assets/js/func.js
+++ assets/js/func.js
@@ -41,3 +41,28 @@ function navToggle() {
 
 toggle();
 
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.carousel-slide');
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove('active');
+    if (i === index) {
+      slide.classList.add('active');
+    }
+  });
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(currentSlide);
+}
+
+// Initialize the first slide
+showSlide(currentSlide);
